c8d68f1204859137e2db4d0b074723bf832639b9
Ronal-Zuniga <ronal.zuniga@unitec.edu>
2024-05-14 14:16:00 -0600

    corregidos métodos de backend

--- API-Laravel/app/Http/Controllers/Api/ToDoController.php
+++ API-Laravel/app/Http/Controllers/Api/ToDoController.php
@@ -4,23 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ToDo;
 
 class ToDoController extends Controller
 {
     
     public function index()
     {
-        
+        $to_dos = ToDo::all();
+        return $to_dos;
     }
 
     public function store(Request $request)
     {
-        
+        $to_do = new ToDo();
+        $to_do->description = $request->description;
+        $to_do->completed = $request->completed;
+        $to_do->save();
     }
 
     public function show(string $id)
     {
-        
+        $to_do = ToDo::findOrFail($id);
+        return $to_do;
     }
 
     public function update(Request $request, string $id)
@@ -32,4 +38,26 @@ class ToDoController extends Controller
     {
         
     }
+
+    public function toggleCompleted(string $id)
+    {
+        $to_do = ToDo::findOrFail($id);
+        $to_do->completed = !$to_do->completed;
+        $to_do->save();
+        return $to_do;
+    }
+
+    public function destroyCompleted()
+    {
+        $to_do = ToDo::where('completed', true)->delete();
+        return $to_do;
+    }
+
+    public function destroyAll()
+    {
+        $to_do = ToDo::truncate();
+        return $to_do;
+    }
 }
+
+
--- API-Laravel/routes/api.php
+++ API-Laravel/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ToDoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::controller(ToDoController::class)->group(function () {
+    Route::get('/to-dos', 'index');
+    Route::post('/to-do', 'store');
+    Route::get('/to-do/{id}', 'show');
+    Route::put('/to-do/{id}/toggle-completed', 'toggleCompleted');
+    Route::delete('/to-dos/completed', 'destroyCompleted');
+    Route::delete('/to-dos', 'destroyAll');
+});
