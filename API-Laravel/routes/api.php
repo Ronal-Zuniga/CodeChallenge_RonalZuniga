faf89697c1182cc0f66f66860a47491638f37262
Ronal-Zuniga <ronal.zuniga@unitec.edu>
2024-05-14 14:53:25 -0600

    probando nuevo enfoque

--- API-Laravel/routes/api.php
+++ API-Laravel/routes/api.php
@@ -9,12 +9,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::resource('/to_dos', ToDoController::class)->only([
+    'index', 'store', 'show', 'update', 'destroy'
+]);
 
-Route::controller(ToDoController::class)->group(function () {
-    Route::get('/to_dos', [ToDoController::class, 'index']);
-    Route::post('/to_dos', [ToDoController::class, 'store']);
-    Route::get('/to_dos/{id}', [ToDoController::class, 'show']);
-    Route::put('/to_dos/{id}/toggle-completed', [ToDoController::class, 'toggleCompleted']);
-    Route::delete('/to_dos/completed', [ToDoController::class, 'destroyCompleted']);
-    Route::delete('/to_dos', [ToDoController::class, 'destroyAll']);
-});
+Route::put('/to_dos/{id}/toggle-completed', [ToDoController::class, 'toggleCompleted']);
+Route::delete('/to_dos/completed', [ToDoController::class, 'destroyCompleted']);
+Route::delete('/to_dos', [ToDoController::class, 'destroyAll']);
